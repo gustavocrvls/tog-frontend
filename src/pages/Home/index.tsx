ccce6ea93af74a7e2226c78fe222d9abb54b684c
Gustavo Carvalho Silva <gustavocrvl42@gmail.com>
2021-05-26 11:26:27 -0300

    Inicio da criação da tela de artigos

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,29 @@
+import { Box, Heading, SimpleGrid } from '@chakra-ui/layout';
+
 export function Home(): JSX.Element {
-  return <div>Home!</div>;
+  return (
+    <Box>
+      <SimpleGrid columns={2} gap="8">
+        <Box backgroundColor="white" borderRadius="xl" padding="5">
+          <img
+            src="https://i.graphicmama.com/blog/wp-content/uploads/2019/10/07142805/graphic-design-trends-2020-breaking-the-rules.jpg"
+            alt="Cover"
+          />
+          <Heading as="h2" size="md">
+            What was the trend in 2020 and you
+          </Heading>
+        </Box>
+        <Box backgroundColor="white" borderRadius="xl" padding="5">
+          <img
+            src="https://i.graphicmama.com/blog/wp-content/uploads/2019/10/07142805/graphic-design-trends-2020-breaking-the-rules.jpg"
+            alt="Cover"
+          />
+          <Heading as="h2" size="md">
+            What was the trend in 2020 and you
+          </Heading>
+        </Box>
+      </SimpleGrid>
+      <div>Menu Lateral</div>
+    </Box>
+  );
 }
